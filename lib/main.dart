46c12d4ba981e0d0ac5dc9224a70bd43b12db229
gaaj1980 <gaaj1980@gmail.com>
2020-08-13 11:08:09 -0300

    -Refactoring nos nomes das classes; -Inclusão do botão reset; -Ajustes nos estilos.

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(DSIApp());
 }
 
-class MyApp extends StatelessWidget {
+class DSIApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,22 +16,22 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'DSI App'),
+      home: DSIPage(title: 'My First App - DSI/BSI/UFRPE'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
+class DSIPage extends StatefulWidget {
   final String title;
 
+  DSIPage({Key key, this.title}) : super(key: key);
+
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _DSIPageState createState() => _DSIPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  var _phrases = [
+class _DSIPageState extends State<DSIPage> {
+  final _warnings = [
     'JÁ DEU BENÇA!',
     'VAI QUEBRAR O CELULAR!',
     'PARA VÉI!',
@@ -46,15 +46,21 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  String _getRandomPhrase() {
-    var idx = Random().nextInt(_phrases.length);
-    return _phrases[idx];
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
   }
 
-  String _getText() {
-    String result = 'Você clicou $_counter vezes no botão.';
+  String _getCountText() {
+    return 'Você clicou $_counter vezes no botão.';
+  }
+
+  String _getWarningText() {
+    String result = '';
     if (_counter > 5) {
-      result += '\n${_getRandomPhrase()}';
+      var idx = Random().nextInt(_warnings.length);
+      result = _warnings[idx];
     }
     return result;
   }
@@ -62,12 +68,14 @@ class _MyHomePageState extends State<MyHomePage> {
   String _getImageName() {
     //abra o arquivo 'pubspec.yaml' e veja as entradas na seção 'assets'.
     //para incluir novas imagens, basta incluir novas entradas nesta seção.
-    String result = '';
-    if (_counter > 5) {
+    String result;
+    if (_counter == 0) {
+      result = '';
+    } else if (_counter > 5) {
       result = 'images/jadeu.png';
     } else if (_counter > 2) {
       result = 'images/thug2.gif';
-    } else if (_counter > 0) {
+    } else {
       result = 'images/thug1.jpg';
     }
     return result;
@@ -84,7 +92,22 @@ class _MyHomePageState extends State<MyHomePage> {
           Image(image: AssetImage('images/logo/bsi-white.png'), height: 32),
         ],
       )),
-      body: Center(child: MyMessageWidget(_getText(), _getImageName())),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+            child: Column(children: <Widget>[
+          DSIMainBodyWidget(
+              _getCountText(), _getWarningText(), _getImageName()),
+          Spacer(),
+          FlatButton(
+              onPressed: _resetCounter,
+              color: Colors.green,
+              child: Text(
+                'Reset',
+                style: TextStyle(color: Colors.white, fontSize: 16.0),
+              ))
+        ])),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
@@ -94,26 +117,33 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class MyMessageWidget extends StatelessWidget {
-  String _text = '';
-  String _image = '';
-  MyMessageWidget(this._text, this._image);
+class DSIMainBodyWidget extends StatelessWidget {
+  final String _countText;
+  final String _warningText;
+  final String _image;
+  DSIMainBodyWidget(this._countText, this._warningText, this._image);
 
   @override
   Widget build(BuildContext context) {
     return Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              _text,
+          Text(
+            _countText,
+            style: TextStyle(color: Colors.black, fontSize: 16.0),
+          ),
+          SizedBox(height: 8.0),
+          if (_warningText.isNotEmpty)
+            Text(
+              _warningText,
+              style: TextStyle(color: Colors.red, fontSize: 24.0),
             ),
-          ),
-          Image(
-            image: AssetImage(_image),
-            height: 180,
-          ),
+          SizedBox(height: 8.0),
+          if (_image.isNotEmpty)
+            Image(
+              image: AssetImage(_image),
+              height: 240,
+            ),
         ]);
   }
 }
