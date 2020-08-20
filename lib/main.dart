1e730f9bfa92fd76f2f39fcf1873516cdea7cb1d
gaaj1980 <gaaj1980@gmail.com>
2020-08-20 05:51:39 -0300

    Ajuste na formatação, com a inclusão das vírgulas faltantes (estilo recomendado pelo Flutter).

--- lib/main.dart
+++ lib/main.dart
@@ -131,24 +131,25 @@ class DSIMainBodyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          _countText,
+          style: TextStyle(color: Colors.black, fontSize: 16.0),
+        ),
+        SizedBox(height: 8.0),
+        if (_warningText.isNotEmpty)
           Text(
-            _countText,
-            style: TextStyle(color: Colors.black, fontSize: 16.0),
+            _warningText,
+            style: TextStyle(color: Colors.red, fontSize: 24.0),
           ),
-          SizedBox(height: 8.0),
-          if (_warningText.isNotEmpty)
-            Text(
-              _warningText,
-              style: TextStyle(color: Colors.red, fontSize: 24.0),
-            ),
-          SizedBox(height: 8.0),
-          if (_image.isNotEmpty)
-            Image(
-              image: AssetImage(_image),
-              height: 240,
-            ),
-        ]);
+        SizedBox(height: 8.0),
+        if (_image.isNotEmpty)
+          Image(
+            image: AssetImage(_image),
+            height: 240,
+          ),
+      ],
+    );
   }
 }
