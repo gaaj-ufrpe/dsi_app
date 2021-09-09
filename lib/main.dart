b48e0b8218395952ccbe1da9522166394729d6d2
gaaj-ufrpe <gabriel.alves@ufrpe.br>
2021-09-09 09:26:00 -0300

    -ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -122,7 +122,7 @@ class _DSIPageState extends State<DSIPage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Incrementar',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
